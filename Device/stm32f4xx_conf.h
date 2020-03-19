e2856e7183ddbae650f65ae65be4a34e668e9f3a
Shawn Chain <shawn.chain@gmail.com>
2020-03-19 14:46:17 +0800

    update conf

--- Device/stm32f4xx_conf.h
+++ Device/stm32f4xx_conf.h
@@ -37,10 +37,10 @@
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_tim.h"
 #include "stm32f4xx_usart.h"
-#include "stm32f10x_i2c.h"
-#include "stm32f10x_iwdg.h"
-#include "stm32f10x_wwdg.h"
-#include "stm32f10x_flash.h"
+#include "stm32f4xx_i2c.h"
+#include "stm32f4xx_iwdg.h"
+#include "stm32f4xx_wwdg.h"
+#include "stm32f4xx_flash.h"
 #include "misc.h" /* High level functions for NVIC and SysTick (add-on to CMSIS functions) */
 
 /* Exported types ------------------------------------------------------------*/
